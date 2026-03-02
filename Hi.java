f78ba08fc1022f3657063f43e78afdd36180f06b
ankurdotdev <vermaankur0600@gmail.com>
2026-03-02 19:47:03 +0530

    updated

--- Hi.java
+++ Hi.java
@@ -1,13 +1,16 @@
 import java.util.*;
-class HiAnshi{
-    public static void main(String[] args) {
-        System.out.println(" HI THIS IS MY FIRST COMMIT OF 2026! ");
-        Scanner i = new Scanner(System.in);
 
+class HiAnkur{
+    public static void main(String[] args) {
+        System.out.println("HI THIS IS MY FIRST COMMIT OF 2026!");
+
+        Scanner i = new Scanner(System.in);
         int a = i.nextInt();
         System.out.println(a);
-        main([1,2,3]);
+
+        main(new int[]{1,2,3});   // ✔ correct way
     }
+
     public static void main(int[] a){
         System.out.println("overloading");
     }
