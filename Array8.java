13948807e41ae2ebac14e0982a1462272192c241
ankurdotdev <vermaankur0600@gmail.com>
2026-03-07 22:08:50 +0530

    Find the Index of an Element

--- Array8.java
+++ Array8.java
@@ -2,7 +2,7 @@
 
 public class Array8 {
     public static void main(String[] args) {
-        int[] a = {1,2,4,3,4,4};
+        int[] a = {1,2,4,3,4};
         int target = 4;
 
         for(int i =0;i<a.length;i++){
