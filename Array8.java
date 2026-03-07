8d3cd99a11a3b3d517c0e6f383212afd4d19b2e9
ankurdotdev <vermaankur0600@gmail.com>
2026-03-07 21:54:24 +0530

    Find the Index of an Element

---
+++ Array8.java
@@ -0,0 +1,13 @@
+public class Array8 {
+    public static void main(String[] args) {
+        int[] a = {1,2,4,3,4,4};
+        int target = 4;
+
+        for(int i =0;i<a.length;i++){
+            if(a[i] == target){
+                System.out.println("Targetted element index--> "+i);
+            }
+
+        }
+    }
+}
